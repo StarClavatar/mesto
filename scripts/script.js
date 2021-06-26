7baf5a5d477ea1d6a5326d0da399a0ebd9090ea6
vyacheslav <clavatar@yandex.ru>
2021-06-26 23:03:06 +0300

    исправления внесены

--- scripts/script.js
+++ scripts/script.js
@@ -3,24 +3,28 @@ let saveButton = popup.querySelector('.popup__save-button')
 let closeButton = popup.querySelector('.popup__close-button')
 let profile = document.querySelector('.profile')
 let popupOpenButton = profile.querySelector('.profile__button_type_edit')
-let popupTitle = profile.querySelector('.profile__title')
-let popupShortDescription = profile.querySelector('.profile__short-description')
+let profileTitle = profile.querySelector('.profile__title')
+let profileShortDescription = profile.querySelector('.profile__short-description')
 let editPopupTitle = popup.querySelector('.popup__input_edit_title')
 let editPopupShortDescription = popup.querySelector('.popup__input_edit_short-description')
 
 
 let openPopup = function () {
-    popup.classList.add('popup__opened');
+    
+    editPopupTitle.value = profileTitle.textContent
+    editPopupShortDescription.value = profileShortDescription.textContent
+
+    popup.classList.add('popup_opened');
 }
 
 let closePopup = function() {
-    popup.classList.remove('popup__opened')
+    popup.classList.remove('popup_opened')
 }
 
 let saveInputText = function(save) {
     save.preventDefault()
-    popupTitle.textContent = editPopupTitle.value
-    popupShortDescription.textContent = editPopupShortDescription.value
+    profileTitle.textContent = editPopupTitle.value
+    profileShortDescription.textContent = editPopupShortDescription.value
     closePopup()
 }
 // https://yandex.ru/video/preview/?filmId=3620754122386298535&from=tabbar&text=submit+eventListener+js
